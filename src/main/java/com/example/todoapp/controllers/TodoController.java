f049d1090deec7f456fb9353cb24254b312c8c10
ilya <ilya.tsygankov.191@mail.ru>
2025-11-14 22:14:29 +0300

    TodoController commit

---
+++ src/main/java/com/example/todoapp/controllers/TodoController.java
@@ -0,0 +1,14 @@
+package com.example.todoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TodoController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
