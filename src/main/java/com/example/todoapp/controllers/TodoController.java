85d403ea8375d39f922cb66771c26f521d22ea8e
ilya <ilya.tsygankov.191@mail.ru>
2025-11-16 14:40:29 +0300

    Remove button commit

--- src/main/java/com/example/todoapp/controllers/TodoController.java
+++ src/main/java/com/example/todoapp/controllers/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -33,6 +34,12 @@ public class TodoController implements CommandLineRunner {
     @PostMapping("/add")
     public String add(@ModelAttribute TodoItem todoItem){
         todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") Long id){
+        todoItemRepository.deleteById(id);
         return "redirect:/";
     }
 
